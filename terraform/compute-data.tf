28a98c196e6c55f1f64d9d1995b51bb77401430a
Max Roulstone <max@the-shack.net>
2025-11-29 22:43:55 +0000

    added: final app

--- terraform/compute-data.tf
+++
@@ -1,4 +0,0 @@
-data "google_compute_image" "container_optimized" {
-  family  = var.vm_image_family
-  project = var.vm_image_project
-}
--- terraform/main.tf
+++
@@ -1,7 +0,0 @@
-provider "google" {
-  project               = var.gcp_project_name
-  region                = var.gcp_region
-  zone                  = var.gcp_zone
-  user_project_override = true
-  billing_project       = var.gcp_billing_project_name
-}
